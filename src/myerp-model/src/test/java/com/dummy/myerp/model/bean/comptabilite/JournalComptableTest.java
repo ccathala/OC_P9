75037cb05126ede65d341226c468aae6361cec22
ccathala <cathala.cha@gmail.com>
2020-04-20 10:25:31 +0200

    Impl test JournalComptable #2

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import static com.dummy.myerp.model.bean.comptabilite.JournalComptable.getByCode;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JournalComptableTest {
@@ -33,5 +36,29 @@ public class JournalComptableTest {
         final String  result = classUnderTest.toString();
         // THEN
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void Given_journalComptableList_When_getByCodeIsUsed_Then_shouldReturnTheSameBean() {
+        // GIVEN
+        List<JournalComptable> journalComptableList = new ArrayList<>();
+        journalComptableList.add(new JournalComptable("AC","Achats"));
+        journalComptableList.add(new JournalComptable("VE","Ventes"));
+        // WHEN
+        final JournalComptable result= getByCode(journalComptableList, "VE");
+        // THEN
+        assertThat(result).isEqualTo(journalComptableList.get(1));
+    }
+
+    @Test
+    public void Given_journalComptableList_When_getByCodeIsUsedWithWrongCode_Then_shouldReturnNull() {
+        // GIVEN
+        List<JournalComptable> journalComptableList = new ArrayList<>();
+        journalComptableList.add(new JournalComptable("AC","Achats"));
+        journalComptableList.add(new JournalComptable("VE","Ventes"));
+        // WHEN
+        final JournalComptable result= getByCode(journalComptableList, "RE");
+        // THEN
+        assertThat(result).isNull();
     }
 }
